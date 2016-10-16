74ab00100d19416dbd27db375139276db0282b1f
vincent chi <vincent.chi@126.com>
2016-10-16 22:09:10 +0800

    master

---
+++ Java/src/hi2.java
@@ -0,0 +1,8 @@
+/**
+ * Created by CBY on 2016/10/16.
+ */
+public class hi2 {
+    public static void main(String[] args) {
+        System.out.println("hi2");
+    }
+}
--- Java/src/yourbranch1.java
+++ Java/src/yourbranch1.java
@@ -1,3 +1,6 @@
 
 public class yourbranch1 {
+    public static void main(String[] args) {
+        System.out.println("yourbranch1");
+    }
 }
